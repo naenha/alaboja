f3be5059e7d72f7a444576a1c90f7a32a0abe3d4
dbsdlqls <yibin99@sookmyung.ac.kr>
2021-02-06 23:47:50 +0900

    주택 모델 생성

--- alaboja/views.py
+++ alaboja/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .forms import HouseInputForm
+from .models import House
 
 # Create your views here.
 def main(request):
@@ -17,9 +18,30 @@ def store(request):
         if not form.is_valid():
             return HttpResponseBadRequest("Invalid Input")
         csv_input = form.cleaned_data['csv_input'].split('\n')
+        header = [ col.strip() for col in csv_input[0].split(',') ]
+        lines = csv_input[1:]
+
+        translate = {}
+        translate['단지명'] = 'name'
+        translate['주소'] = 'address'
+        translate['공급유형'] = 'category'
+        translate['공급면적'] = 'area'
+        translate['세대수'] = 'num'
+        translate['가구원수'] = 'people'
+        translate['공급대상'] = 'target'
+        translate['소득분위'] = 'rating'
+        translate['소득/자산'] = 'income'
+
+        for line in lines:
+            if line.count(',') != len(header) - 1:
+                continue
+            options = {}
+            for idx, token in enumerate(line.split(',')):
+                options[translate[header[idx]]] = token.strip()
+            house = House.objects.get_or_create(**options)
 
     form = HouseInputForm()
-    
+
     return render(request, 'alaboja/store.html', {
         'form': form,
     })
